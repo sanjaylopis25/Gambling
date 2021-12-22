2794458c1ebb2af8b0804bef2e633840ae170f17
sanajylopis25 <sanajylopis25@gmail.com>
2021-12-22 09:45:58 +0530

    UC1 Add

---
+++ Gambling/Gambling/UC1Stake.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gambling
+{
+    class UC1Stake
+    {
+        int stake = 100;
+        const int BET_PER_DAY = 1;
+    }
+}
